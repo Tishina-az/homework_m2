51eeb665a9517f94ba4f018d21e14377e32799c9
Andrey Zashchepin <tishina-az@mail.ru>
2024-09-22 13:31:25 +0500

    optimization of functions

--- src/widget.py
+++ src/widget.py
@@ -3,16 +3,15 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(name_account_card: str) -> str:
     """Функция возвращает строку с замаскированным номером счета или карты"""
-    mask_number = ""
-    numbers = name_account_card.split()
+
+    mask_number: str = ""
+    numbers: list[str] = name_account_card.split()
     for num in numbers:
         if num.isdigit():
             if len(num) == 16:
-                mask_num = get_mask_card_number(num)
-                mask_number += mask_num
+                mask_number += get_mask_card_number(num)
             else:
-                mask_num = get_mask_account(num)
-                mask_number += mask_num
+                mask_number += get_mask_account(num)
         else:
             mask_number += num + " "
 
@@ -21,4 +20,5 @@ def mask_account_card(name_account_card: str) -> str:
 
 def get_date(date: str) -> str:
     """Функция, которая возвращает корректную дату вида "ДД.ММ.ГГГГ" ("11.03.2024")"""
+
     return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
