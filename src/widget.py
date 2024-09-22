592e6e2d743589162fda99cf6f7201fb7b4f7f52
Andrey Zashchepin <tishina-az@mail.ru>
2024-09-22 13:14:10 +0500

    finaly change

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,7 @@ from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(name_account_card: str) -> str:
-    """ Функция возвращает строку с замаскированным номером счета или карты """
+    """Функция возвращает строку с замаскированным номером счета или карты"""
     mask_number = ""
     numbers = name_account_card.split()
     for num in numbers:
@@ -15,16 +15,10 @@ def mask_account_card(name_account_card: str) -> str:
                 mask_number += mask_num
         else:
             mask_number += num + " "
-            # mask_number += " "
 
     return mask_number
 
 
-# print(mask_account_card('Visa Classic 6831982476737658')) - проверка кода
-
 def get_date(date: str) -> str:
-    """ Функция, которая возвращает корректную дату вида "ДД.ММ.ГГГГ" ("11.03.2024") """
-    return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
-
-
-# print(get_date('2024-03-11T02:26:18.671407')) - проверка кода
+    """Функция, которая возвращает корректную дату вида "ДД.ММ.ГГГГ" ("11.03.2024")"""
+    return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
