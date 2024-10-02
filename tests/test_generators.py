57c0187808abc61a782dd181a2911ecb483ef526
Andrey Zashchepin <tishina-az@mail.ru>
2024-10-02 16:25:36 +0500

    add tests for transaction

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,4 +1,6 @@
-from src.generators import filter_by_currency
+import pytest
+
+from src.generators import filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency(transactions: list, transactions_usd: list, transactions_rub: list) -> None:
@@ -19,3 +21,20 @@ def test_filter_by_currency_empty() -> None:
     """Тестирование функции на обработку случаев, когда транзакции в заданной валюте отсутствуют"""
     result = list(filter_by_currency([], 'AED'))
     assert result == []
+
+
+def test_transaction_descriptions(transactions: list) -> None:
+    """Тестирование функции на возврат корректного описания каждой транзакции"""
+    generator = transaction_descriptions(transactions)
+    assert next(generator) == 'Перевод организации'
+    assert next(generator) == 'Перевод со счета на счет'
+    assert next(generator) == 'Перевод со счета на счет'
+    assert next(generator) == 'Перевод с карты на карту'
+    assert next(generator) == 'Перевод организации'
+
+
+def test_transaction_descriptions_values(transactions_rub: list) -> None:
+    """Тестирование функции на пустой список"""
+    with pytest.raises(StopIteration):
+        assert next(transaction_descriptions([])) == []
+        assert next(transaction_descriptions([])) == []
