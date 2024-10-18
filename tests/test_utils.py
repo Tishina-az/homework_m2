fc033de649868fed05fd30c66e02c2b277084863
Andrey Zashchepin <tishina-az@mail.ru>
2024-10-18 17:28:29 +0500

    fix test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,12 +7,15 @@ from src.utils import read_file_transactions
 
 @patch('builtins.open')
 def test_read_file_transactions(mock_open):
+    """Тестирует поведение функции при различных вариантах содержания файла"""
     path = os.path.join(os.path.dirname(__file__), 'data/operations.json')
-    path_wrong = os.path.join(os.path.dirname(__file__), 'data/operation.json')
     mock_file = mock_open.return_value.__enter__.return_value
+    # корректные данные
     mock_file.read.return_value = json.dumps([{'login': 'testuser', 'name': 'Test User'}])
     assert read_file_transactions(path) == [{'login': 'testuser', 'name': 'Test User'}]
+    # данные не соответствуют условиям, не список
     mock_file.read.return_value = json.dumps({'login': 'testuser', 'name': 'Test User'})
     assert read_file_transactions(path) == []
+    # пустой файл
     mock_file.read.return_value = ''
     assert read_file_transactions(path) == []
