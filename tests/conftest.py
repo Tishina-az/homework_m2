aaa16b10a3ac76140e4f33aa4966fef8636512e2
Andrey Zashchepin <tishina-az@mail.ru>
2024-10-30 16:04:16 +0500

    add tests new function

--- tests/conftest.py
+++ tests/conftest.py
@@ -284,3 +284,49 @@ def transactions_rub() -> list:
             "to": "Счет 14211924144426031657"
         }
     ]
+
+
+@pytest.fixture
+def transactions_org() -> list[dict]:
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+
+
+@pytest.fixture
+def category():
+    return ["Перевод организации"]
+
+
+@pytest.fixture
+def category_dict():
+    return {"Перевод организации": 2}
