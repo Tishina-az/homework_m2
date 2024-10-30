38c4bc85c78b91bc159ac220b65e787e0345f920
Andrey Zashchepin <tishina-az@mail.ru>
2024-10-30 13:58:34 +0500

    add filter by search

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,22 @@
-from typing import Iterable, Optional
+import re
+from typing import Iterable
 
 
-def filter_by_state(list_state: Iterable[dict], state: Optional[str] = 'EXECUTED') -> list[dict]:
+def filter_by_state(list_state: list[dict], state: str = 'EXECUTED') -> list[dict]:
     """ Функция возвращает новый список словарей, содержащий только те словари,
     у которых ключ state соответствует указанному значению """
 
-    return [i for i in list_state if i['state'] == state]
+    return [i for i in list_state if i.get('state').lower() == state.lower()]
 
 
 def sort_by_date(list_date: Iterable[dict], sort_dir: bool = True) -> list[dict]:
     """ Функция возвращает список словарей, отсортированный по дате """
 
     return sorted(list_date, key=lambda i: i['date'], reverse=sort_dir)
+
+
+def filter_by_search_line(list_transactions: list[dict], search_line: str) -> list[dict]:
+    """ Функция возвращает список словарей, содержащих строку поиска в описании """
+
+    # return [i for i in list_transactions if search_line.lower() in i.get('description').lower()]
+    return [i for i in list_transactions if re.search(search_line, i.get('description'), flags=re.IGNORECASE)]
